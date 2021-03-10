e13f4f5eb296e28c0a1c55660bb5ca48196e210e
Glenn <glenn.y.liu@gmail.com>
2021-03-09 22:34:38 -0500

    Separated preprocessing for rcp85 and historical

--- preproc_CESM.py
+++ preproc_CESM.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 
 datpath = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/downloaded/ocn/proc/tseries/monthly/SSH/"
 outpath = "/stormtrack/data3/glliu/01_Data/03_SeaLevelProject/SSH/Regridded/"
-mconfig = "HTR"
+mconfig = "R85"
 mnum    = np.hstack([np.arange(1,36,1),np.arange(101,108,1)])
 
 latlonf = "/home/glliu/01_Data/cesm_latlon360.npz"
@@ -55,50 +55,115 @@ def fix_febstart(ds):
         ds = ds.assign_coords(time=correctedtime) 
     return ds
 
-#%%
-# Get NC Files
-ncstring = "b.e11.B20TRC5CNBDRD.f09_g16.*.pop.h.SSH.*.nc"
-nclist = glob.glob(datpath+ncstring)
-nclist.sort()
-nclist = [fn for fn in nclist if "OIC" not in fn]
-nfiles = len(nclist)
-print("Found %i files" % (nfiles))
+#%% # Get NC Files (historical Period)
+if mconfig=="HTR":
+    ncstring = "b.e11.B20TRC5CNBDRD.f09_g16.*.pop.h.SSH.*.nc"
+    ncount   = 42 
 
-# Set up lat/lon target grid
-ds_out = xr.Dataset({'lat': (['lat'], lat), 'lon': (['lon'], lon) })
-dy = lat[1]-lat[0]
-dx = lon[1]-lon[0]
-dx,dy
-#ds_out = xe.util.grid_global(dx,dy)
-
-for n in tqdm(range(nfiles)):
-    # Open and Load Dataset
-    ncname = nclist[n]
-    ds = xr.open_dataset(ncname)
-    ds = preprocess(ds,varlist=varkeep)
-    ds = fix_febstart(ds)
+    nclist = glob.glob(datpath+ncstring)
+    nclist.sort()
+    nclist = [fn for fn in nclist if "OIC" not in fn]
+    nfiles = len(nclist)
+    print("Found %i files" % (nfiles))
     
-    # Visualize first timestep and grid
-    if debug:
-        ds.SSH.isel(time=0).plot()
-        plt.show()
+    # Set up lat/lon target grid
+    ds_out = xr.Dataset({'lat': (['lat'], lat), 'lon': (['lon'], lon) })
+    dy = lat[1]-lat[0]
+    dx = lon[1]-lon[0]
+    
+    for n in tqdm(range(nfiles)):
         
-        plt.scatter(ds['TLONG'], ds['TLAT'], s=0.01)
-        plt.show()
+        # Open and Load Dataset
+        ncname = nclist[n]
+        ds = xr.open_dataset(ncname)
+        ds = preprocess(ds,varlist=varkeep)
+        ds = fix_febstart(ds)
+        
+        # Visualize first timestep and grid
+        if debug:
+            ds.SSH.isel(time=0).plot()
+            plt.show()
+            
+            plt.scatter(ds['TLONG'], ds['TLAT'], s=0.01)
+            plt.show()
+        
+        # Rename lat/lon
+        ds = ds.rename({'TLONG': 'lon', 'TLAT': 'lat'})
+        
+        # Make regridder  and Regrid (set periodic to true to get rid of strip along lon=0)
+        regridder = xe.Regridder(ds, ds_out, 'bilinear',periodic=True)
+        ds_regrid = regridder(ds.SSH)
+        
+        # Visualize result
+        if debug:
+            ds_regrid.isel(time=0).plot()
+            plt.show()
+        
+        # Save the output
+        outname = outpath + "SSH_%s_ens%02d.nc" % (mconfig,n+1)
     
-    # Rename lat/lon
-    ds = ds.rename({'TLONG': 'lon', 'TLAT': 'lat'})
-    
-    # Make regridder  and Regrid (set periodic to true to get rid of strip along lon=0)
-    regridder = xe.Regridder(ds, ds_out, 'bilinear',periodic=True)
-    ds_regrid = regridder(ds.SSH)
-    # Visualize result
-    if debug:
-        ds_regrid.isel(time=0).plot()
-        plt.show()
-    
-    # Save the output
-    outname = outpath + "SSH_HTR_ens%02d.nc" % (n+1)
-    ds_regrid.to_netcdf(outname,encoding={'SSH': {'zlib': True}})   
+        ds_regrid.to_netcdf(outname,encoding={'SSH': {'zlib': True}})   
 
 
+#%% Get NC files (RCP85)
+
+if mconfig=="R85":
+        ncstring = "b.e11.BRCP85C5CNBDRD.f09_g16.*.pop.h.SSH.*.nc"
+        ncount   = 40
+    nclist = glob.glob(datpath+ncstring)
+    nclist.sort()
+    nclist = [fn for fn in nclist if "OIC" not in fn]
+    nfiles = len(nclist)
+    print("Found %i files" % (nfiles))
+    
+    # Set up lat/lon target grid
+    ds_out = xr.Dataset({'lat': (['lat'], lat), 'lon': (['lon'], lon) })
+    dy = lat[1]-lat[0]
+    dx = lon[1]-lon[0]
+    
+    
+    e = 0
+    concatflag = False
+    for n in tqdm(range(nfiles)):
+        
+        # Open and Load Dataset
+        ncname = nclist[n]
+        ds = xr.open_dataset(ncname)
+        ds = preprocess(ds,varlist=varkeep)
+        ds = fix_febstart(ds)
+        
+        # Visualize first timestep and grid
+        if debug:
+            ds.SSH.isel(time=0).plot()
+            plt.show()
+            
+            plt.scatter(ds['TLONG'], ds['TLAT'], s=0.01)
+            plt.show()
+        
+        # Rename lat/lon
+        ds = ds.rename({'TLONG': 'lon', 'TLAT': 'lat'})
+        
+        # Make regridder  and Regrid (set periodic to true to get rid of strip along lon=0)
+        regridder = xe.Regridder(ds, ds_out, 'bilinear',periodic=True)
+        ds_regrid = regridder(ds.SSH)
+        # Visualize result
+        if debug:
+            ds_regrid.isel(time=0).plot()
+            plt.show()
+        
+        # Save the output
+        if mconfig == 'R85':
+            if ds_regrid.time.values[-1].year != 2100:
+                # Save file and reoeat process for the second file
+                ds_save = ds_regrid.copy()
+                concatflag = True
+                concatname = ncname
+                continue
+            
+            if concatflag is True:
+                print("Concatenating %s and %s" % (concatname,ncname))
+                ds_regrid = xr.concat([ds_save,ds_regrid],dim='time')
+                concatflag = False
+        
+        ds_regrid.to_netcdf(outname,encoding={'SSH': {'zlib': True}})   
+        e+=1
