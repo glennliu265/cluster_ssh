07dc799fa30b2d15ca5f2271712d7dc3d76f27dc
Glenn <glenn.y.liu@gmail.com>
2021-06-04 02:07:04 -0400

    Save cluster_CESM-PIC before edits

--- cluster_CESM-PIC.py
+++ cluster_CESM-PIC.py
@@ -340,6 +340,9 @@ times = ds.time.values
 ntime,nlat5,nlon5 = ssh.shape
 print("Loaded data in %.2fs"%(time.time()-st))
 
+# Set up time array
+timesmon = np.array(["%04d-%02d"%(t.year,t.month) for t in times])
+
 # Plotting utilities
 cmbal = cmocean.cm.balance
 
@@ -405,84 +408,6 @@ if add_gmsl:
 else:
     print("No GMSL Added!")
 
-#%%
-
-
-
-
-# #%% Additional Preprocessing Steps
-
-# # -----------------------------------------
-# # Limit to particular period (CESM Version)
-# # -----------------------------------------
-# Convert Datestrings
-timesmon = np.array(["%04d-%02d"%(t.year,t.month) for t in times])
-
-# # Find indices
-# idstart  = np.where(timesmon==start)[0][0]
-# idend    = np.where(timesmon==end)[0][0]
-
-# # Restrict Data to period
-# ssh     = ssh[idstart:idend,:,:]
-# timeslim = timesmon[idstart:idend]
-# timesyr  = np.array(["%04d"%(t.year) for t in times])[idstart:idend]
-# ntimer   = ssh.shape[0]
-
-# # -------------------------
-# # Remove the Long Term Mean
-# # -------------------------
-# ssha = ssh - ssh.mean(0)[None,:,:]
-# if debug: # Plot Results of mean removal
-#     fig,axs = plt.subplots(2,1,figsize=(8,8))
-#     ax = axs[0]
-#     pcm = ax.pcolormesh(lon5,lat5,ssh[0,:,:])
-#     fig.colorbar(pcm,ax=ax)
-#     ax.set_title("SSH")
-    
-#     ax = axs[1] 
-#     pcm = ax.pcolormesh(lon5,lat5,ssha[0,:,:],cmap=cmbal)
-#     fig.colorbar(pcm,ax=ax)
-#     ax.set_title("SSH Anomaly (Long Term Mean Removed")
-
-
-
-# # ------------------------------
-# # Filter Again, If Option is Set
-# # ------------------------------
-# if filteragain:
-#     slasmooth = np.zeros((ntimer,nlat5,nlon5))
-#     for i in tqdm(range(ntimer)):
-#         da = xr.DataArray(ssha[i,:,:].astype('float32'),
-#                         coords={'lat':lat5,'lon':lon5},
-#                         dims={'lat':lat5,'lon':lon5},
-#                         name='sla')
-#         timestamp = times[i]
-#         smooth_field = pygmt.grdfilter(grid=da, filter="g500", distance="4",nans="i")
-#         slasmooth[i,:,:] = smooth_field.values
-    
-    
-#     # Reapply Mask to correct for smoothed edges
-#     mask = ssha.sum(0)
-#     mask[~np.isnan(mask)] = 1
-#     sla_filt = slasmooth * mask[None,:,:]
-
-
-#     if debug:
-#         fig,axs = plt.subplots(2,1,figsize=(8,8))
-#         ax = axs[0]
-#         pcm = ax.pcolormesh(lon5,lat5,ssha[0,:,:],cmap=cmbal,vmin=-30,vmax=30)
-#         fig.colorbar(pcm,ax=ax)
-#         ax.set_title("SSHA Before Filtering")
-        
-#         ax = axs[1] 
-#         pcm = ax.pcolormesh(lon5,lat5,sla_filt[0,:,:],cmap=cmbal,vmin=-30,vmax=30)
-#         fig.colorbar(pcm,ax=ax)
-#         ax.set_title("SSHA After Filtering")
-        
-        
-
-    
-
 
 # ----------------------
 #%% Design Low Pass Filter
@@ -541,70 +466,7 @@ if savesteps: # Save low-pass-filtered result, right before clustering
         'lat':lat5,
         'times':times
         })
-
-
-
-
-
-
     
-#%% Cluster in moving windows
-
-sla_in = sla_lp[:,:,:]
-maxiter = 6
-
-# Get ranges for sliding window
-winsize = 240
-#rngs = []
-clusters   = []
-uncerts    = []
-counts     = []
-rempts     = []
-Wks        = []
-s_all      = []
-s_by_clust = []
-for i in tqdm(range(ntime-240)):
-    rng = np.arange(i,i+winsize+1)
-    sla_in = sla_lp[rng,:,:]
-    
-    #allclusters,alluncert,allcount,rempt,allWk = elim_points(sla_in,lat5,lon5,nclusters,minpts,maxiter,expdir,
-    #                                                         viz=False,printmsg=False)
-    
-    
-    allclusters,alluncert,allcount,rempt,allWk,alls,alls_byclust = elim_points(sla_in,lat5,lon5,nclusters,minpts,maxiter,expdir,
-                                                             viz=False,printmsg=False,calcsil=True)
-    
-    
-    clusters.append(np.array(allclusters[-1])) # Save, and just take the final result # [niter x nlat x nlon]
-    uncerts.append(np.array(alluncert[-1]))    # [niter x nlat x nlon]
-    counts.append(np.array(allcount[-1]))      # [niter x nclusters]
-    rempts.append(np.array(rempt))             # [lat x lon]
-    Wks.append(np.array(allWk))                # [niter x nclusters]
-    s_all.append(np.array(alls[-1]))           # [niter x nanpts]
-    s_by_clust.append(alls_byclust[-1])        # [niter x ncluster]
-    
-    # End loop
-
-# Make everything to arrays
-
-
-
-np.savez("%s%s_Results_winsize%i.npz"%(datpath,expname,winsize),**{
-    'lon':lon5,
-    'lat':lat5,
-    'sla':sla_lp,
-    'clusters':clusters,
-    'uncert':uncerts,
-    'count':counts,
-    'rempts':rempts,
-    'Wks':Wks,
-    's_all':s_all,
-    's_by_clust':s_by_clust},allow_pickle=True)
-
-test = clusters[0]
-
-
-
 
 #%% Making the Regiondict
 from scipy import stats
